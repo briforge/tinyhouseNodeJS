1de23f2522c9a16b2322a10f0ae980205a5a211f
Brian Preston <Brian.Preston@salliemae.com>
2020-09-04 23:52:09 -0400

    latest elint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,29 @@
 import express from "express";
+import { listings } from "./listings";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 9000;
 
-const one:number = 1;
-const two:number = 2;
+
+app.use(bodyParser.json());  // they say you could also use express.json() as well
 
 
-app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
+app.get('/listings', (_req, res) => {
+    res.send(listings);
+})
+
+app.post('/delete-listing', (req, res) => {
+    const id: string = req.body.id;
+
+    for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+            return res.send(listings.splice(i, 1));
+        }
+    }
+
+    return res.send("failed to delete listing; did not find matching id");
+})
 
 app.listen(port);
 
